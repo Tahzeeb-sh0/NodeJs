e9af1a30e7c84f37ccb99138ddffd0d54701a5d8
Tahzeeb-sh0 <taizibshaikh34@gmail.com>
2025-03-13 01:21:55 +0530

    Add basic math operations and example usage

---
+++ 1_hello.js
@@ -0,0 +1,7 @@
+
+const math = require("./math.js") 
+
+console.log("Value",math.add(1,2))
+console.log("Value",math.mul(1,2))
+console.log("Value",math.div(1,2))
+console.log("Value",math.sub(1,2))
