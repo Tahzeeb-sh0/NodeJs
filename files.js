3a8f4214fd794c99e77cbbc3c915f1f42ff0d3ca
Tahzeeb-sh0 <taizibshaikh34@gmail.com>
2025-03-13 02:24:32 +0530

    Add file system operations for reading, writing, and managing files

---
+++ files.js
@@ -0,0 +1,31 @@
+const fs = require('fs')
+// console.log(fs)
+
+//Sync
+fs.writeFileSync("./text.txt"," hello! ");
+
+// //Async
+// fs.writeFile("./text.txt","hello there",(error)=>error);
+
+
+const text = fs.readFileSync("./text.txt" ,"utf-8")
+console.log(text);
+
+fs.readFile("./text.txt","utf-8",(err,result)=>{
+    if (!err) {
+        console.log(result)
+    }else{
+        console.log(err)
+    }
+})
+
+const date = fs.appendFileSync("./text.txt", new Date().getDate().toLocaleString())
+console.log(date);
+
+fs.cpSync("./text.txt","./cp.txt")
+
+fs.unlinkSync("./cp.txt")
+
+console.log(fs.statSync("./text.txt").isFile())
+
+fs.mkdirSync("Document")
