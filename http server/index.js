70b3129b12e955bb6892a2a713017658201cffb7
Tahzeeb-sh0 <taizibshaikh34@gmail.com>
2025-03-14 01:20:08 +0530

    Enhance HTTP server logging to include request methods and add signup route handling

--- http server/index.js
+++ http server/index.js
@@ -7,7 +7,7 @@ const myserver = http.createServer((req, res) => {
         return res.end();
     }
 
-  const log = `${Date.now()}: ${req.url} new req received\n`;
+  const log = `${Date.now()}: ${req.url}  ${req.method}new req received\n`;
   const urls = url.parse(req.url,true);
   console.log(urls)
   fs.appendFile("text.txt", log, (err, data) => {
@@ -21,11 +21,15 @@ const myserver = http.createServer((req, res) => {
         res.end("my details");
         break;
 
-        case "/search":
-            const searchQuery = urls.query.search_query
-            res.end(`Here your searched result ${searchQuery}`);
-            break;
+      case "/search":
+        const searchQuery = urls.query.search_query
+        res.end(`Here your searched result ${searchQuery}`);
+        break;
+      case "/signup":
+        if(req.method === "GET") res.end(`This is a signup page`);
+        else if (req.method === "POST") res.end(`Success`);
 
+        break;
       default:
         res.end("404 not found");
         break;
