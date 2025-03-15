e96177d60d3411fb91097def7d843321b47407ba
Tahzeeb-sh0 <taizibshaikh34@gmail.com>
2025-03-15 14:06:45 +0530

    Add middleware for logging and enhance user creation response handling

--- Rest Api/index.js
+++ Rest Api/index.js
@@ -2,19 +2,26 @@ const express = require('express');
 const users = require('./MOCK_DATA.json')
 const app = express();
 const fs = require('fs');
-const { json } = require('stream/consumers');
 PORT = 7000;
 
 
 app.use(express.urlencoded({extended:false}))
 
+app.use((req,res,next)=>{
+  console.log("hello from middleware")
+    next()
+})
+
 app.route('/api/users')
 .get((req, res) => {
     // const html = `
+
     // <ol>
     // ${users.map(users=>`<li>${users['first-name']}</li>`).join('\n')}
     // </ol>
     // `
+
+    res.setHeader("x-name","tahzeeb")
     res.json(users)
 })
 .patch((req , res)=>{
@@ -27,7 +34,11 @@ app.route('/api/users')
 
     const body = req.body;
     users.push({...body , id: users.length + 1})
-    fs.writeFile('./MOCK_DATA.json', JSON.stringify(users),(err,data)=>{})
+    fs.writeFile('./MOCK_DATA.json', JSON.stringify(users),(err,data)=>{
+        if (err) throw err;
+        console.log('Data written to file');
+        res.json({status:'success'})
+    })
    
 })
 
