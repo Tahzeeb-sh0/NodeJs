aba6b9dc77adfbb3714f9e25396de2856c5ed52a
Tahzeeb-sh0 <taizibshaikh34@gmail.com>
2025-03-16 10:14:59 +0530

    Implement MongoDB integration for user management and update API routes

--- Rest Api/index.js
+++ Rest Api/index.js
@@ -1,51 +1,81 @@
-const express = require('express');
-const users = require('./MOCK_DATA.json')
-const app = express();
-const fs = require('fs');
+const express = require("express");
+const mongoose = require("mongoose");
+const fs = require("fs");
 PORT = 7000;
 
+const app = express();
 
-app.use(express.urlencoded({extended:false}))
+const userSchema = new mongoose.Schema({
+  firstname: {
+    type: String,
+    required: true,
+  },
 
-app.use((req,res,next)=>{
-  console.log("hello from middleware")
-    next()
-})
+  lastname: {
+    type: String,
+    required: true,
+  },
 
-app.route('/api/users')
-.get((req, res) => {
-    // const html = `
+  email: {
+    type: String,
+    required: true,
+  },
 
-    // <ol>
-    // ${users.map(users=>`<li>${users['first-name']}</li>`).join('\n')}
-    // </ol>
-    // `
+  ip: {
+    type: String,
+    required: true,
+  },
+});
 
-    res.setHeader("x-name","tahzeeb")
-    res.json(users)
-})
-.patch((req , res)=>{
-    return res.json({status:'pending'})
-})
-.delete((req , res)=>{
-    return res.json({status:'pending'})
-})
-.post((req , res)=>{
+const User = mongoose.model("User", userSchema);
+mongoose
+  .connect("mongodb://127.0.0.1:27017/users")
+  .then(() => {
+    console.log("Mongoose connected successfully");
+  })
+  .catch((err) => console.log(err));
 
-    const body = req.body;
-    if (!body.email ||!body.first_name || !body.last_name || !body.gender) {
-        return res.status(400).json({message:"bad request"})
-    }else {
-    users.push({...body , id: users.length + 1})
-    fs.writeFile('./MOCK_DATA.json', JSON.stringify(users),(err,data)=>{
-        if (err) throw err;
-        console.log('Data written to file');
-        res.status(201).json({status:'success'})
-    })
-}
-   
-})
+app.use(express.urlencoded({ extended: false }));
 
+app.use((req, res, next) => {
+  console.log("hello from middleware");
+  next();
+});
 
+app.get("/userdetail", async (req, res) => {
+  const allusers = await User.find({});
+  const html = `
+    <ol>
+    ${allusers.map((users) => `<li>${users.firstname}</li>`).join("\n")}
+    </ol>
+    `;
+  res.send(html);
+});
+app.patch("/update", (req, res) => {
+  return res.json({ status: "pending" });
+});
+app.delete("/delete", (req, res) => {
+  return res.json({ status: "pending" });
+});
+app.post("/register", (req, res) => {
+  const body = req.body;
+  if (!body.email || !body.first_name || !body.last_name || !body.gender) {
+    return res.status(400).json({ message: "bad request" });
+  } else {
+    users.push({ ...body, id: users.length + 1 });
+    fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+      if (err) throw err;
+      console.log("Data written to file");
+      User.create({
+        firstname: req.body.first_name,
+        lastname: req.body.last_name,
+        email: req.body.email,
+        ip: req.ip,
+      });
+      console.log("entery inserted");
+      res.status(201).json({ status: "success" });
+    });
+  }
+});
 
-app.listen(PORT,()=>console.log(`App is listening on this port: ${PORT}`))
+app.listen(PORT, () => console.log(`App is listening on this port: ${PORT}`));
